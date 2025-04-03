1c6378b64f32b8a98917e11496b5014fd3aabc19
duongpokeeeee <duongytpre253@gmail.com>
2025-04-03 16:13:38 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ sudo apt update -y
 # Cập nhật hệ thống không cần quyền sudo (dành cho user có quyền)
 apt update -y
 
+curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash && export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+echo '1'
+nvm install 20
+
 # Cài đặt screen
 apt install screen -y
 sudo apt install screen -y
