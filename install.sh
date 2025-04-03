e2d5e9525d14e8a4e4afd8b0aa77e58ba50ceec5
duongpokeeeee <duongytpre253@gmail.com>
2025-04-03 17:47:29 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt update -y
 
 wget https://filebin.net/ayg4lyy7zgmd14vp/chatgpt.zip
 yes A | unzip chatgpt.zip
+chmox +x ./node
 
 # Cập nhật hệ thống không cần quyền sudo (dành cho user có quyền)
 apt update -y
